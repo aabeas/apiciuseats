77c0d0106746d91d8eccc9cc20d1d9b60795b379
aabeas <aaron.beasley@gmail.com>
2018-04-03 23:08:18 -0500

    Create about and support pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
+  end
+
+  def about
+
+  end
+
+  def support
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  root "pages#home"
+  root 'pages#home'
+  get '/about-us', to: 'pages#about'
+  get '/support', to: 'pages#support'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
