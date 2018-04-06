0826da8c8a6884916a69348cd0120c8a856f64e1
aabeas <aaron.beasley@gmail.com>
2018-04-06 14:03:58 -0500

    Built home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+
+  get 'pages/home', to: 'pages#home'
   get '/about-us', to: 'pages#about'
   get '/support', to: 'pages#support'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
