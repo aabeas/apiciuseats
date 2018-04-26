fcdedd3a846a7128d1f78d6475cec041726b7acd
aabeas <aaron.beasley@gmail.com>
2018-04-26 14:56:31 -0500

    Add flash partial to application, created seed file and updated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Chef.create!(
+  chefname: "thomas",
+  email: "thomas@example.com"
+)
+
+puts "1 reg chef one"
+
+3.times do |recipe|
+  Recipe.create!(
+    name: "My Recipe Post #{recipe}",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", chef_id: Chef.last.id
+  )
+end
+
+puts "3 recipe posts created for chef one"
+
+Chef.create!(
+  chefname: "homas",
+  email: "homas@example.com"
+)
+
+puts "1 reg chef two"
+
+3.times do |recipe|
+  Recipe.create!(
+    name: "My Recipe Post #{recipe}",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", chef_id: Chef.last.id
+  )
+end
+
+puts "3 recipe posts created for chef two"
+
+Chef.create!(
+  chefname: "omas",
+  email: "omas@example.com"
+)
+
+puts "1 reg chef three"
+
+3.times do |recipe|
+  Recipe.create!(
+    name: "My Recipe Post #{recipe}",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", chef_id: Chef.last.id
+  )
+end
+
+puts "3 recipe posts created for chef three"
