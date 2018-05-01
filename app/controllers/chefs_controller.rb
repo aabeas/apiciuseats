c96615788e5f4cdc68b3fc80c402c3611e5035af
aabeas <aaron.beasley@gmail.com>
2018-05-01 02:44:18 -0500

    Create chefs show action with tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,5 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show]
 
   def new
     @chef = Chef.new
@@ -15,13 +16,16 @@ class ChefsController < ApplicationController
   end
 
   def show
-
   end
 
   private
 
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+
+  def set_chef
+    @chef = Chef.find(params[:id])
   end
 
 end
