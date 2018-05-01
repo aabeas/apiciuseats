245a1a7c3d7f8837c45141a60cc8fe06c8a58417
aabeas <aaron.beasley@gmail.com>
2018-05-01 00:58:56 -0500

    Implement chef signup with tests

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -9,11 +9,24 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
 
   test "reject an invalid signup" do
     get signup_path
-    assert_response :success
+    assert_no_difference "Chef.count" do
+      post chefs_path, params: { chef: { chefname: " ", email: " ",
+                                 password: "password", password_confirmation: " " } }
+    end
+    assert_template 'chefs/new'
+    assert_select 'h5.card-title'
+    assert_select 'div.card-body'
   end
 
   test "accept valid signup" do
-    
+    get signup_path
+    assert_difference "Chef.count", 1 do
+      post chefs_path, params: { chef: { chefname: "mas", email: "mas@example.com",
+                                 password: "password", password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_not flash.empty?
   end
 
 end
